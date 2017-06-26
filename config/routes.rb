5ca527a071b9bc9f1510c09ac948226e3ca817a8
Aaron Johnson <aaronjohnsonis@gmail.com>
2017-06-26 15:27:49 -0500

    contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'homes', to: 'pages#available'
   get 'detail', to: 'pages#detail'
   get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
