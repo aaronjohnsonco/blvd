d9be979b04de78e2daf60b14dda0ac75abd54e84
Aaron Johnson <aaronjohnsonis@gmail.com>
2017-06-30 10:00:07 -0500

    dropdown nav

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'detail', to: 'pages#detail'
   get 'contact', to: 'pages#contact'
   get 'about', to: 'pages#about'
+  get 'communities', to: 'pages#communities'
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
