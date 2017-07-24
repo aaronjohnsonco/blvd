92223fbc07252d9f1bea6427b4b2f14bcfb15900
Aaron Johnson <aaronjohnsonis@gmail.com>
2017-07-24 14:38:15 -0500

    forms

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,6 +8,16 @@ class ContactsController < ApplicationController
       ContactMailer.thank_you_email(@contact).deliver
       redirect_to root_path
       flash[:notice] = "Your message has been sent."
+    elsif params[:contact_btn] && @contact.save
+      ContactMailer.contact_general_email(@contact).deliver
+      ContactMailer.thank_you_email(@contact).deliver
+      redirect_to root_path
+      flash[:notice] = "Your message has been sent."
+    elsif params[:warranty_btn] && @contact.save
+      ContactMailer.contact_warranty_email(@contact).deliver
+      ContactMailer.thank_you_email(@contact).deliver
+      redirect_to root_path
+      flash[:notice] = "Your message has been sent."
     else
       redirect_to root_path
       flash[:notice] = "Your email did not go through, please try again."
@@ -27,7 +37,8 @@ class ContactsController < ApplicationController
       :state,
       :zip,
       :closing_date,
-      :message
+      :message,
+      :community
     )
   end
 
