3fd3103727e15d5750561728d60bfa6eb155ca13
Aaron Johnson <aaronjohnsonis@gmail.com>
2017-07-28 14:28:09 -0500

    home links

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,22 +2,22 @@ class ContactMailer < ApplicationMailer
 
   def thank_you_email(contact)
     @contact = contact
-    mail(to: contact.email, from: 'aaronjohnsonis@gmail.com', subject: 'Thanks for contacting us!')
+    mail(to: contact.email, from: ['amanda@blvdhomes.com', 'aaronj@bigmoondev.com'], subject: 'Thanks for contacting us!')
   end
 
   def contact_general_email(contact)
     @contact = contact
-    mail(from: contact.email, to: 'aaronjohnsonis@gmail.com', subject: 'New General Contact')
+    mail(from: contact.email, to: ['amanda@blvdhomes.com', 'aaronj@bigmoondev.com'], subject: 'New General Contact')
   end
 
   def contact_warranty_email(contact)
     @contact = contact
-    mail(from: contact.email, to: 'aaronjohnsonis@gmail.com', subject: 'New Warranty Page Contact')
+    mail(from: contact.email, to: ['amanda@blvdhomes.com', 'aaronj@bigmoondev.com'], subject: 'New Warranty Page Contact')
   end
 
   def contact_home_email(contact)
     @contact = contact
-    mail(from: contact.email, to: ['aaronjohnsonis@gmail.com', 'aaronj@bigmoondev.com'], subject: 'New Home Page Contact')
+    mail(from: contact.email, to: ['amanda@blvdhomes.com', 'aaronj@bigmoondev.com'], subject: 'New Home Page Contact')
   end
 
 end
