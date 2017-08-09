904188599b122bb7800e8c7afa74b19f0209b01f
Aaron Johnson <aaronjohnsonis@gmail.com>
2017-08-09 11:31:09 -0500

    add recaptcha

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,17 +3,17 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    if params[:home_btn] && @contact.save
+    if verify_recaptcha(model: @contact) && params[:home_btn] && @contact.save
       ContactMailer.contact_home_email(@contact).deliver
       ContactMailer.thank_you_email(@contact).deliver
       redirect_to root_path
       flash[:notice] = "Your message has been sent."
-    elsif params[:contact_btn] && @contact.save
+    elsif verify_recaptcha(model: @contact) && params[:contact_btn] && @contact.save
       ContactMailer.contact_general_email(@contact).deliver
       ContactMailer.thank_you_email(@contact).deliver
       redirect_to root_path
       flash[:notice] = "Your message has been sent."
-    elsif params[:warranty_btn] && @contact.save
+    elsif verify_recaptcha(model: @contact) && params[:warranty_btn] && @contact.save
       ContactMailer.contact_warranty_email(@contact).deliver
       ContactMailer.thank_you_email(@contact).deliver
       redirect_to root_path
